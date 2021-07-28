cf85712cd07f2e3580a40c0e8a0a9774b7009592
DESKTOP-MJV0GUE\Thiti <thiti180536@gmail.com>
2021-07-29 01:08:27 +0700

    change api req path

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiPath = 'https://covid19.th-stat.com/api/open/today';
+const apiPath = 'https://covid19.th-stat.com/json/covid19v2/getTodayCases.json';
 
 export const fetchData = async () => {
   try {
